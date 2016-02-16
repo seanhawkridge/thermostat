19736ed4ebfba719a2444e60e8cd883f7a15e865
itsindigo <matthewbridgesbusiness@gmail.com>
2016-02-16 14:58:41 +0000

    added powersave mode and upper temp limits + tests

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -23,4 +23,30 @@ describe("Thermostat", function() {
     expect(thermo.temperature).toEqual(19)
   });
 
+  it("Should throw an error if .down is called at 10 degrees.", function(){
+    thermo.temperature = 10;
+    expect(function () {thermo.down();}).toThrow(new Error ('Temp too low'));
+  });
+
+  it("Should change value of powersaving mode", function(){
+    thermo.powerSaveOn()
+    expect(thermo.powerSavingMode).toBe(true);
+  });
+
+  it("Should change value of powersaving mode", function(){
+    thermo.powerSaveOff()
+    expect(thermo.powerSavingMode).toBe(false);
+  });
+
+  it("Should raise error at 33 temp if powersave off", function(){
+    thermo.temperature = 32;
+    expect(function () {thermo.up();}).toThrow(new Error ('Max temperature reached'));
+  });
+
+  it("Should raise error at 26 temp if powersave on",function(){
+    thermo.powerSaveOn()
+    thermo.temperature = 25;
+    expect(function () {thermo.up();}).toThrow(new Error ('Max temperature reached'));
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -5,8 +5,26 @@ function Thermostat() {
 };
 
 Thermostat.prototype.up = function() {
-  this.temperature += 1;
+  if((this.powerSavingMode === false && this.temperature < 32)||(this.powerSavingMode === true && this.temperature < 25))
+    this.temperature += 1;
+  else {
+    throw new Error('Max temperature reached');
+  };
 };
+
 Thermostat.prototype.down = function() {
-  this.temperature -= 1;
+  if(this.temperature > 10){
+    this.temperature -= 1;
+  }
+  else {
+    throw new Error('Temp too low');
+  };
+};
+
+Thermostat.prototype.powerSaveOn = function () {
+    this.powerSavingMode = true;
+};
+
+Thermostat.prototype.powerSaveOff = function () {
+    this.powerSavingMode = false;
 };
